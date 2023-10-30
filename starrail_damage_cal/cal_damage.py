4514b0578194c84653070e5da844fb20e2683d26
qwerdvd <105906879+qwerdvd@users.noreply.github.com>
2023-10-30 15:25:15 +0800

    add

--- starrail_damage_cal/cal_damage.py
+++ starrail_damage_cal/cal_damage.py
@@ -14,6 +14,20 @@ from starrail_damage_cal.mono.Character import Character
 from starrail_damage_cal.to_data import api_to_dict
 
 
+async def cal_char_info(char_data: Dict):
+    char: Character = Character(char_data)
+    await char.get_equipment_info()
+    await char.get_char_attribute_bonus()
+    await char.get_relic_info()
+    return char
+
+
+async def cal_info(char_data: Dict):
+    char = await cal_char_info(char_data)
+    avatar = AvatarInstance(char)
+    return await avatar.get_damage_info()
+
+
 class DamageCal:
     @classmethod
     async def cal_info(cls, char_data: Dict):
